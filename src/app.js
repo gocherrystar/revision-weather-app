b502383f458341c4220ac4856cd97314d66feb18
gocherrystar <79139792+gocherrystar@users.noreply.github.com>
2023-03-21 20:22:56 +1100

    Added relevant icon from the api

--- src/app.js
+++ src/app.js
@@ -16,22 +16,27 @@ let day = days[date.getDay()];
 }
 
 function displayTemperature(response) {
+    
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = response.data.temperature.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.time * 1000);
+    iconElement.setAttribute("src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`); 
+    iconElement.setAttribute("alt", response.data.condition.description);
 }
 
 let apikey = "3c6eof27af57b08t26d5a0a1f6f02cc4";
-let apiurl = 'https://api.shecodes.io/weather/v1/current?query=Sydney&key=3c6eof27af57b08t26d5a0a1f6f02cc4&units=metric';    
+let city = "Sydney";
+let apiurl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=3c6eof27af57b08t26d5a0a1f6f02cc4&units=metric`;    
 
 
 axios.get(apiurl).then(displayTemperature);
